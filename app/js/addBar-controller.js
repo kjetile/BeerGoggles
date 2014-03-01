64275ccc2a738c9c8258ec0395e3a5234e5c1340
Kjetil Eide <kjetile445@gmail.com>
2014-03-01 13:09:36 +0100

    WIP: submit new bar

--- app/js/addBar-controller.js
+++ app/js/addBar-controller.js
@@ -2,7 +2,13 @@
 
 var addBarController = angular.module('addBarController', []);
 
-addBarController.controller('AddBarController', ['$scope',
-    function ($scope) {
-
-    }]);
+addBarController.controller('AddBarController', ['$scope', '$http',
+    function ($scope, $http) {
+        $scope.addBar = function (barData) {
+            $http({
+                method  : 'POST',
+                url     : insert url to scroogeAPI here
+            })
+        };
+    }
+ ]);
